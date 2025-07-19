737b9f48a9184ad10320e0a5b20c0ca899735d16
m.kiselev <sanwiraight@gmail.com>
2025-07-19 13:27:57 +0300

    [Sprint 24] Create submodules

---
+++ core/comment-service/src/main/java/ru/practicum/ewm/commentservice/CommentService.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewm.commentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentService {
+    public static void main(String[] args) {
+        SpringApplication.run(CommentService.class);
+    }
+}
---
+++ core/request-service/src/main/java/ru/practicum/ewm/requestservice/RequestService.java
@@ -0,0 +1,9 @@
+package ru.practicum.ewm.requestservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class RequestService {
+    public static void main(String[] args) {
+        SpringApplication.run(RequestService.class);
+    }
+}
